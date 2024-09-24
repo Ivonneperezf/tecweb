afa073b565ec48823bc171a897a027efa9a511d3
Ivonne <ivonne.perezf@alumno.buap.mx>
2024-09-24 10:44:08 -0600

    Se creo el archivo get_productos_vigentes.php

--- practicas/p09/get_productos_vigentes.php
+++ practicas/p09/get_productos_vigentes.php
@@ -1,49 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Productos</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+<h2>Lista de Productos</h2>
+
 <?php
-    header("Content-Type: application/json; charset=utf-8"); 
     $data = array();
 
-	if(isset($_GET['tope']))
-    {
-		$tope = $_GET['tope'];
+    /** SE CREA EL OBJETO DE CONEXION */
+    @$link = new mysqli('localhost', 'root', 'Mivida243@.', 'marketzone');
+    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+
+    /** comprobar la conexión */
+    if ($link->connect_errno) {
+        die('Falló la conexión: '.$link->connect_error.'<br/>');
     }
-    else
-    {
-        die('Parámetro "tope" no detectado...');
-    }
 
-	if (!empty($tope))
-	{
-		/** SE CREA EL OBJETO DE CONEXION */
-		@$link = new mysqli('localhost', 'root', 'Mivida243@.', 'marketzone');
-        /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+    /** Crear una tabla que no devuelve un conjunto de resultados */
+    if ($result = $link->query("SELECT * FROM productos WHERE eliminado = 0")) {
+        /** Se extraen las tuplas obtenidas de la consulta */
+        $row = $result->fetch_all(MYSQLI_ASSOC);
 
-		/** comprobar la conexión */
-		if ($link->connect_errno) 
-		{
-			die('Falló la conexión: '.$link->connect_error.'<br/>');
-			//exit();
-		}
-
-		/** Crear una tabla que no devuelve un conjunto de resultados */
-		if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= $tope") ) 
-		{
-            /** Se extraen las tuplas obtenidas de la consulta */
-			$row = $result->fetch_all(MYSQLI_ASSOC);
-
-            /** Se crea un arreglo con la estructura deseada */
-            foreach($row as $num => $registro) {            // Se recorren tuplas
-                foreach($registro as $key => $value) {      // Se recorren campos
-                    $data[$num][$key] = utf8_encode($value);
-                }
+        /** Se crea un arreglo con la estructura deseada */
+        foreach ($row as $num => $registro) {
+            foreach ($registro as $key => $value) {
+                $data[$num][$key] = $value;  // Se asume que los datos ya están en UTF-8
             }
+        }
 
-			/** útil para liberar memoria asociada a un resultado con demasiada información */
-			$result->free();
-		}
+        /** útil para liberar memoria asociada a un resultado con demasiada información */
+        $result->free();
+    }
 
-		$link->close();
+    $link->close();
+?>
 
-        /** Se devuelven los datos en formato JSON */
-        echo json_encode($data, JSON_PRETTY_PRINT);
-	}
-	?>
+<table class="table">
+    <thead class="thead-dark">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Marca</th>
+            <th scope="col">Modelo</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Unidades</th>
+            <th scope="col">Detalles</th>
+            <th scope="col">Imagen</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($data as $index => $row) { ?>
+            <tr>
+                <th scope="row"><?= $index + 1 ?></th>
+                <td><?= htmlspecialchars($row['nombre']) ?></td>
+                <td><?= htmlspecialchars($row['marca']) ?></td>
+                <td><?= htmlspecialchars($row['modelo']) ?></td>
+                <td><?= htmlspecialchars($row['precio']) ?></td>
+                <td><?= htmlspecialchars($row['unidades']) ?></td>
+                <td><?= htmlspecialchars($row['detalles']) ?></td>
+                <td><img src="<?= htmlspecialchars($row['imagen']) ?>" alt="Imagen de producto" width="100"></td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
+</body>
+</html>
