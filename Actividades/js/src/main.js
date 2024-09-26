32f05e1f2c57a10ed1af3dfe0390b513ca9a5618
Ivonne <ivonne.perezf@alumno.buap.mx>
2024-09-26 14:25:16 -0600

    Se realizo el ejemplo 3

--- Actividades/js/src/main.js
+++ Actividades/js/src/main.js
@@ -26,3 +26,15 @@ function variablesSinPrompt(){
     document.write( '<br>' );
     document.write( casado );
 }
+
+function variablesConPrompt(){
+    var nombre;
+    var edad;
+    nombre = prompt('Ingresa tu nombre:', '');
+    edad = prompt('Ingresa tu edad:', '');
+    document.write('Hola ');
+    document.write(nombre);
+    document.write(' así que tienes ');
+    document.write(edad);
+    document.write(' años');
+}
