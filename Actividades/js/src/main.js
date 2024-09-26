19e261480647abe2a2494e5dde5509ecfbb43003
Ivonne <ivonne.perezf@alumno.buap.mx>
2024-09-26 14:30:21 -0600

    Se realizo el ejemplo 4

--- Actividades/js/src/main.js
+++ Actividades/js/src/main.js
@@ -38,3 +38,17 @@ function variablesConPrompt(){
     document.write(edad);
     document.write(' años');
 }
+
+function proEstructurada(){
+    var valor1;
+    var valor2;
+    valor1 = prompt('Introducir primer número:', '');
+    valor2 = prompt('Introducir segundo número', '');
+    var suma = parseInt(valor1)+parseInt(valor2);
+    var producto = parseInt(valor1)*parseInt(valor2);
+    document.write('La suma es ');
+    document.write(suma);
+    document.write('<br>');
+    document.write('El producto es ');
+    document.write(producto);
+}
