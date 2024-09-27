64ce94db11ed577346aded7bcd048350d74baffd
Ivonne <ivonne.perezf@alumno.buap.mx>
2024-09-27 08:06:38 -0600

    Se realizo el ejemplo 11

--- Actividades/js/src/main.js
+++ Actividades/js/src/main.js
@@ -144,3 +144,16 @@ function cicloWhile(){
         x=x+1;
     }
 }
+
+function acumulador(){
+    var x=1;
+    var suma=0;
+    var valor;
+    while (x<=5){
+        valor = prompt('Ingresa el valor:', '');
+        valor = parseInt(valor);
+        suma = suma+valor;
+        x = x+1;
+    }
+    document.write("La suma de los valores es "+suma+"<br>");
+}
