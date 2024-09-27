8ba11afe2464702084bd6907f7f5b83ee13ecd71
Ivonne <ivonne.perezf@alumno.buap.mx>
2024-09-27 08:28:38 -0600

    Se realizo el ejemplo 17

--- Actividades/js/src/main.js
+++ Actividades/js/src/main.js
@@ -217,3 +217,25 @@ function funcionConDatosEntrada(){
         valor2 = parseInt(valor2);
         mostrarRango(valor1,valor2);
 }
+
+function convertirCastellano(x) {
+    if(x==1)
+        return "uno";
+    else if(x==2)
+        return "dos";
+    else if(x==3)
+        return "tres";
+    else if(x==4)
+        return "cuatro";
+    else if(x==5)
+        return "cinco";
+    else
+        return "valor incorrecto";
+}
+
+function funcionRetornoVal(){
+        var valor = prompt("Ingresa un valor entre 1 y 5", "");
+        valor = parseInt(valor);
+        var r = convertirCastellano(valor);
+        document.write(r);
+}
