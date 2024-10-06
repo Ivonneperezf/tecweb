faf2d612fcf5b0373b6f6b8a8be4ceae554207ee
Ivonne <ivonne.perezf@alumno.buap.mx>
2024-10-06 16:25:20 -0600

    Se cambiaron los nombres de los archivos

--- practicas/p10/get_productos_xhtml_v2.php
+++ practicas/p10/get_productos_xhtml_v2.php
@@ -46,7 +46,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>Productos</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <script src = "./js/main.js"></script>
+        <script src = "./js/edicionTabla.js"></script>
 </head>
 <body>
     <h2>Lista de Productos</h2>
