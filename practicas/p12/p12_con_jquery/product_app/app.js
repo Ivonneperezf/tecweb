c22fff62ac19b3195bd741b6b1817e7e6a49e9ce
Ivonne <ivonne.perezf@alumno.buap.mx>
2024-10-21 19:46:14 -0600

    Se agrego la funcion de busqueda

--- practicas/p12/p12_con_jquery/product_app/app.js
+++ practicas/p12/p12_con_jquery/product_app/app.js
@@ -15,7 +15,42 @@ function init() {
      */
     var JsonString = JSON.stringify(baseJSON,null,2);
     document.getElementById("description").value = JsonString;
-
+    buscarProducto();
     // SE LISTAN TODOS LOS PRODUCTOS
     listarProductos();
+}
+
+function listarProductos(){
+    
+}
+
+function buscarProducto() {
+    $('#product-result').hide();
+    $('#search').keyup(function(e) {
+        let search = $('#search').val();
+        if (!search) {
+            $('#container').empty();  
+            $('#product-result').hide();  
+            return; 
+        }
+        $.ajax({
+            url: 'backend/product-search.php',
+            type: 'GET',
+            data: { search },
+            success: function(response) {
+                let products = JSON.parse(response);
+                let template = '';
+                products.forEach(product => {
+                    template += `<li>${product.nombre}</li>`;
+                });
+
+                $('#container').html(template);
+                if (template) {
+                    $('#product-result').show();
+                } else {
+                    $('#product-result').hide();
+                }
+            }
+        });
+    });
 }
